147efc51ccc16d3de90505138504eb01d89a0fdc
iqbalhanafi <hanafi20ti@mahasiswa.pcr.ac.id>
2021-07-09 14:32:40 +0700

    Membuat Koneksi

--- ProjectPPBO_Kel7/src/koneksi/Konfig.java
+++ ProjectPPBO_Kel7/src/koneksi/Konfig.java
@@ -1,7 +1,23 @@
+
 package koneksi;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Konfig {
-    public static void main(String[] args) {
-        System.out.println("asda");
+    private static Connection MySQLConfig;
+    public static Connection configDB()throws SQLException{
+        try {
+            String url="jdbc:mysql://localhost:3309/projectppbo"; //url database
+            String user="root"; //user database
+            String pass=""; //password database
+            
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            MySQLConfig=DriverManager.getConnection(url, user, pass);
+        } catch (SQLException e) {
+            System.out.println("koneksi gagal " + e.getMessage()); //perintah menampilkan error pada koneksi
+        }
+        return MySQLConfig;
     }
 }
