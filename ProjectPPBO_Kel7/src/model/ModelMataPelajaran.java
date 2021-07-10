a05524a38e15dff08544bc4a6c719d2a3c5b2c1c
iqbalhanafi <hanafi20ti@mahasiswa.pcr.ac.id>
2021-07-10 13:47:20 +0700

    membuat model mata pelajaran (tampil,input,edit,hapus)

--- ProjectPPBO_Kel7/src/model/ModelMataPelajaran.java
+++ ProjectPPBO_Kel7/src/model/ModelMataPelajaran.java
@@ -7,13 +7,17 @@ import javax.swing.table.DefaultTableModel;
 import koneksi.Konfig;
 
 public class ModelMataPelajaran {
-
-    public void tampilSiswa(JTable varTable) {
+ JTable varTable;
+    
+    public ModelMataPelajaran(JTable varTable){
+        this.varTable = varTable;
+    }
+    public void tampilMapel() {
         try {
             DefaultTableModel model = new DefaultTableModel();
-            model.addColumn("Kode Mata Pelajaran");
-            model.addColumn("Nama Mata Pelajaran");
-            model.addColumn("Jam Mata Pelajaran");
+            model.addColumn("Kode");
+            model.addColumn("Nama");
+            model.addColumn("Jam");
             String sql = "SELECT * FROM mapel";
             java.sql.Connection conn = (Connection) Konfig.configDB();
             java.sql.Statement stm = conn.createStatement();
@@ -29,6 +33,47 @@ public class ModelMataPelajaran {
             varTable.setModel(model);
         } catch (Exception e) {
             System.out.println("Error message:" + e);
+        }
+    }
+      public void inputSiswa(String kode, String nama, String jam){
+        try{
+            String sql = "INSERT INTO mapel VALUES('"+kode+"',"
+                    +"'"+nama+"',"
+                    +"'"+jam+"')";
+            java.sql.Connection conn = (Connection) Konfig.configDB();
+            java.sql.PreparedStatement pstm = conn.prepareStatement(sql);
+            pstm.execute();
+            JOptionPane.showMessageDialog(null, "Simpan data baru berhasil!");
+            tampilMapel();
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, "Error: "+e);
+        }
+    }
+    public void editMapel(String kode, String nama, String jam){
+        try{
+            String sql = "UPDATE mapel SET nama='"+nama+"',"
+                    +"jam='"+jam+"'"
+                    +"WHERE kode='"+kode+"'";
+            java.sql.Connection conn = (Connection)Konfig.configDB();
+            java.sql.PreparedStatement pstm = conn.prepareStatement(sql);
+            pstm.execute();
+            JOptionPane.showMessageDialog(null, "Update data berhasil");
+            tampilMapel();
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
+    
+    public void hapusSiswa(String kode){
+        try{
+            String sql = "DELETE FROM mapel WHERE kode='"+kode+"'";
+            java.sql.Connection conn = (Connection)Konfig.configDB();
+            java.sql.PreparedStatement pstm = conn.prepareStatement(sql);
+            pstm.execute();
+            JOptionPane.showMessageDialog(null, "Hapus data berhasil");
+            tampilMapel();
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, e.getMessage());
         }
     }
 }
